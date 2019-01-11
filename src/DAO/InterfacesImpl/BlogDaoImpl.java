e60ee4c5e96070ccedb9bbb84aa156736f69460c
Anass-ELGHAOUI <elghaoui.anass@gmail.com>
2019-01-11 16:11:13 +0100

    updat .gitignore file

--- src/DAO/InterfacesImpl/BlogDaoImpl.java
+++ src/DAO/InterfacesImpl/BlogDaoImpl.java
@@ -3,10 +3,37 @@ package DAO.InterfacesImpl;
 import DAO.Interfaces.BlogDao;
 import Models.Blog;
 
+import java.util.List;
+
 public class BlogDaoImpl implements BlogDao {
 
     @Override
     public Blog insertBlog(Blog blog) {
         return null;
+    }
+
+    @Override
+    public List<Blog> FetchBlogByTitle(String title) {
+        return null;
+    }
+
+    @Override
+    public Blog fetchBlogById(int idBlog) {
+        return null;
+    }
+
+    @Override
+    public List<Blog> getAllBlogs() {
+        return null;
+    }
+
+    @Override
+    public Blog updateBlog(Blog blog) {
+        return null;
+    }
+
+    @Override
+    public void deleteBlog(int idBlog) {
+
     }
 }
