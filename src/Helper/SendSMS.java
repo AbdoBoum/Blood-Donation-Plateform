19644edbb24172605d31d08cb570b2f6389d2668
Anass-ELGHAOUI <elghaoui.anass@gmail.com>
2019-01-22 19:55:22 +0100

    adding sms api

--- src/Helper/SendSMS.java
+++ src/Helper/SendSMS.java
@@ -1,34 +1,36 @@
 package Helper;
 
+import Models.Donnateur;
+import com.nexmo.client.NexmoClient;
+import com.nexmo.client.sms.SmsSubmissionResponse;
+import com.nexmo.client.sms.SmsSubmissionResponseMessage;
+import com.nexmo.client.sms.messages.TextMessage;
+
+import java.util.List;
+
 public class SendSMS {
-    private final String API_KEY = "f3357b4e";
-    private final String API_SECRET = "nr95Yg4Q4QKCAZ14";
 
-      public static void main(String[] args) throws Exception {
 
-        /*  AuthMethod auth = new TokenAuthMethod(API_KEY, API_SECRET);
-          NexmoClient client = new NexmoClient(auth);
+      public static void sendSMS(List<Donnateur> donnateurList, String message) throws Exception {
+          String API_KEY = "f3357b4e";
+          String API_SECRET = "nr95Yg4Q4QKCAZ14";
 
-          TextMessage message = new TextMessage(FROM_NUMBER, TO_NUMBER, "Hello from Nexmo!");
-
-          //There may be more than one response if the SMS sent is more than 160 characters.
-          SmsSubmissionResult[] responses = client.getSmsClient().submitMessage(message);
-            for (SmsSubmissionResult response : responses) {
-            System.out.println(response);
-          } */
-        /*
           NexmoClient client = new NexmoClient.Builder()
                   .apiKey(API_KEY)
                   .apiSecret(API_SECRET)
                   .build();
 
-          SmsSubmissionResponse responses = client.getSmsClient().submitMessage(new TextMessage(
-                  FROM_NUMBER,
-                  TO_NUMBER,
-                  "Hello from Nexmo!"));
-          for (SmsSubmissionResponseMessage response : responses.getMessages()) {
-              System.out.println(response);
+          for(Donnateur donnateur : donnateurList){
+              SmsSubmissionResponse responses = client.getSmsClient().submitMessage(new TextMessage(
+                      "BLOOD DONATION",
+                      donnateur.getTeleDonnateur(),
+                      message));
+              for (SmsSubmissionResponseMessage response : responses.getMessages()) {
+                  System.out.println(response);
+              }
           }
-          */
+
+
+
       }
   }
