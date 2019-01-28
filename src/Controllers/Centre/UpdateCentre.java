a5b21f214ba909e93e30d327af434a46984070ce
AbdoBoum <abderrahim.boumahdi@um5s.net.ma>
2019-01-29 00:59:32 +0100

    fixing hamza and anass code

--- src/Controllers/Centre/UpdateCentre.java
+++ src/Controllers/Centre/UpdateCentre.java
@@ -29,138 +29,137 @@ public class UpdateCentre extends HttpServlet {
     @Override
     public void init() throws ServletException {
         super.init();
-        daoFactory=DAOFactory.getInstance();
-        centreDao=daoFactory.getCentreDaoImpl();
-        villeDao=daoFactory.getVilleDaoImpl();
+        daoFactory = DAOFactory.getInstance();
+        centreDao = daoFactory.getCentreDaoImpl();
+        villeDao = daoFactory.getVilleDaoImpl();
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        session=request.getSession();
-        if(session.getAttribute("admin")==null&&session.getAttribute("centre")==null){
+        session = request.getSession();
+        if (session.getAttribute("admin") == null && session.getAttribute("centre") == null) {
             response.sendRedirect("/login");
-        }else{
-            isCentre=session.getAttribute("centre")==null? false:true;
-            String idCentre=request.getParameter("id");
-            Centre centre=centreDao.getCentre(Integer.parseInt(idCentre));
-            if(centre==null){
-                if(!isCentre)
-                request.setAttribute("flashMessageFaild","Wrong Identifiant");
+        } else {
+            isCentre = session.getAttribute("centre") == null ? false : true;
+            String idCentre = request.getParameter("id");
+            Centre centre = centreDao.getCentre(Integer.parseInt(idCentre));
+            if (centre == null) {
+                if (!isCentre)
+                    request.setAttribute("flashMessageFaild", "Wrong Identifiant");
 
-                this.getServletContext().getRequestDispatcher(isCentre?"/index.jsp":"/jsp/adminDashBoard.jsp").forward(request,response);
-            }else{
-                if(isCentre){
-                    Centre centreConnecter=(Centre)session.getAttribute("centre");
-                    if(centreConnecter.getIdCentre()!=centre.getIdCentre()){
-                        this.getServletContext().getRequestDispatcher("/index.jsp").forward(request,response);
+                this.getServletContext().getRequestDispatcher(isCentre ? "/index.jsp" : "/jsp/adminDashBoard.jsp").forward(request, response);
+            } else {
+                if (isCentre) {
+                    Centre centreConnecter = (Centre) session.getAttribute("centre");
+                    if (centreConnecter.getIdCentre() != centre.getIdCentre()) {
+                        this.getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
                         return;
                     }
                 }
 //            Admin admin=(Admin) session.getAttribute("admin");
-            String nomCentre=request.getParameter("nom");
-            String emailCentre=request.getParameter("email");
-            //juste pour passer les validation de password si admin est connecter
-            String passwordCentre="passwordCentre";
-            if (isCentre){
-                passwordCentre=request.getParameter("password");
-            }
-            String addresseCentre=request.getParameter("addresse");
-            String strIdVille=request.getParameter("ville");
-            String gsm=request.getParameter("tele");
-            // validaton des champs
-            System.out.println(idCentre+" "+nomCentre+" "+emailCentre+" "+passwordCentre+" "+addresseCentre+" "+strIdVille+" "+gsm);
-            if(nomCentre.trim().isEmpty()||emailCentre.trim().isEmpty()||addresseCentre.trim().isEmpty()
-                    ||strIdVille.trim().isEmpty()||gsm.trim().isEmpty()){
-                request.setAttribute("centre",centre);
-                request.setAttribute("flashMessageFaild","Please complete the required fields");
-                returnFormulaire(request,response);
-            }else{
-                //validation pour chaque champs
-                String errors="";
-                errors+=validationField(emailCentre,"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[a-z]{2,6}$","Email is invalid <br>");
-                if (isCentre && !passwordCentre.trim().isEmpty()) {
-                    errors += validationField(passwordCentre, "^.{6,30}$", "Error must have beetwen 6 and 30 caracter<br>");
+                String nomCentre = request.getParameter("nom");
+                String emailCentre = request.getParameter("email");
+                //juste pour passer les validation de password si admin est connecter
+                String passwordCentre = "passwordCentre";
+                if (isCentre) {
+                    passwordCentre = request.getParameter("password");
                 }
-                errors+=validationField(gsm,"^(\\+212|0)[0-9]{9}$","Number gsm is invalid<br>");
-                if(!errors.isEmpty()){
-                    request.setAttribute("flashMessageFaild",errors);
-                    request.setAttribute("centre",centre);
-                    returnFormulaire(request,response);
-                }else{
-
-                    centre.setNameCentre(nomCentre);
-                    centre.setEmailCentre(emailCentre);
-                    if (isCentre && !passwordCentre.trim().isEmpty()){
-                    centre.setPasswordCentre(Utile.stringToSha256(passwordCentre));
+                String addresseCentre = request.getParameter("addresse");
+                String strIdVille = request.getParameter("ville");
+                String gsm = request.getParameter("tele");
+                // validaton des champs
+                System.out.println(idCentre + " " + nomCentre + " " + emailCentre + " " + passwordCentre + " " + addresseCentre + " " + strIdVille + " " + gsm);
+                if (nomCentre.trim().isEmpty() || emailCentre.trim().isEmpty() || addresseCentre.trim().isEmpty()
+                        || strIdVille.trim().isEmpty() || gsm.trim().isEmpty()) {
+                    request.setAttribute("centre", centre);
+                    request.setAttribute("flashMessageFaild", "Please complete the required fields");
+                    returnFormulaire(request, response);
+                } else {
+                    //validation pour chaque champs
+                    String errors = "";
+                    errors += validationField(emailCentre, "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[a-z]{2,6}$", "Email is invalid <br>");
+                    if (isCentre && !passwordCentre.trim().isEmpty()) {
+                        errors += validationField(passwordCentre, "^.{6,30}$", "Error must have beetwen 6 and 30 caracter<br>");
                     }
-                    centre.setAdresseCentre(addresseCentre);
-//                    centre.setIdAdmin(admin.getIdAdmin());
-                    centre.setIdVille(Integer.parseInt(strIdVille));
-                    centre.setTeleCentre(gsm);
+                    errors += validationField(gsm, "^(\\+212|0)[0-9]{9}$", "Number gsm is invalid<br>");
+                    if (!errors.isEmpty()) {
+                        request.setAttribute("flashMessageFaild", errors);
+                        request.setAttribute("centre", centre);
+                        returnFormulaire(request, response);
+                    } else {
 
-                    if(centreDao.updateCentre(centre)){
-                        request.setAttribute("flashMessageSuccess","Centre has been updated");
-                        if(isCentre){
-                            centre=centreDao.getCentre(Integer.parseInt(idCentre));
-                            session.setAttribute("centre", centre);
+                        centre.setNameCentre(nomCentre);
+                        centre.setEmailCentre(emailCentre);
+                        if (isCentre && !passwordCentre.trim().isEmpty()) {
+                            centre.setPasswordCentre(Utile.stringToSha256(passwordCentre));
                         }
-                        this.getServletContext().getRequestDispatcher(isCentre?"/index.jsp":"/jsp/adminDashBoard.jsp").forward(request,response);
-                    }else {
-                        request.setAttribute("flashMessageFaild", "Error updating centre");
-                        this.getServletContext().getRequestDispatcher(isCentre?"/index.jsp":"/jsp/adminDashBoard.jsp").forward(request, response);
-                    }
+                        centre.setAdresseCentre(addresseCentre);
+//                    centre.setIdAdmin(admin.getIdAdmin());
+                        centre.setIdVille(Integer.parseInt(strIdVille));
+                        centre.setTeleCentre(gsm);
 
+                        if (centreDao.updateCentre(centre)) {
+                            request.setAttribute("flashMessageSuccess", "Centre has been updated");
+                            if (isCentre) {
+                                centre = centreDao.getCentre(Integer.parseInt(idCentre));
+                                session.setAttribute("centre", centre);
+                            }
+                            this.getServletContext().getRequestDispatcher(isCentre ? "/index.jsp" : "/jsp/adminDashBoard.jsp").forward(request, response);
+                        } else {
+                            request.setAttribute("flashMessageFaild", "Error updating centre");
+                            this.getServletContext().getRequestDispatcher(isCentre ? "/index.jsp" : "/jsp/adminDashBoard.jsp").forward(request, response);
+                        }
+
+                    }
                 }
             }
-        }
         }
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // return formullaire centre
-        session=request.getSession();
+        session = request.getSession();
 
-        if(session.getAttribute("admin")==null&&session.getAttribute("centre")==null){
+        if (session.getAttribute("admin") == null && session.getAttribute("centre") == null) {
             response.sendRedirect("/login");
-        }
-        else{
-            Boolean isCentre=session.getAttribute("centre")==null? false  : true;
-            List<Centre> centres=centreDao.getAllCentre();
-            request.setAttribute("centres",centres);
+        } else {
+            Boolean isCentre = session.getAttribute("centre") == null ? false : true;
+            List<Centre> centres = centreDao.getAllCentre();
+            request.setAttribute("centres", centres);
 
-            if(!request.getParameterMap().containsKey("update")){
-                request.setAttribute("flashMessageFaild","Erreur link !");
-                    this.getServletContext().getRequestDispatcher(isCentre?"/index.jsp":"/jsp/adminDashBoard.jsp").forward(request,response);
-
-
-            }else{
+            if (!request.getParameterMap().containsKey("update")) {
+                request.setAttribute("flashMessageFaild", "Erreur link !");
+                this.getServletContext().getRequestDispatcher(isCentre ? "/index.jsp" : "/jsp/adminDashBoard.jsp").forward(request, response);
 
 
-                String strIdCentre=request.getParameter("update").trim();
-                String error="";
-                error+=validationField(strIdCentre,"^\\d+$","Identifiant must be numerique");
+            } else {
 
-                if(!error.equals("")){
-                    request.setAttribute("flashMessageFaild","Identifiant must be number");
-                    this.getServletContext().getRequestDispatcher(isCentre?"/index.jsp":"/jsp/adminDashBoard.jsp").forward(request,response);
-                }else{
-                    int idCentre=Integer.parseInt(strIdCentre);
-                    Centre centre=centreDao.getCentre(idCentre);
-                    if(isCentre){
-                        Centre centreConnecter=(Centre)session.getAttribute("centre");
-                        if(centreConnecter.getIdCentre()!=centre.getIdCentre()){
+
+                String strIdCentre = request.getParameter("update").trim();
+                String error = "";
+                error += validationField(strIdCentre, "^\\d+$", "Identifiant must be numerique");
+
+                if (!error.equals("")) {
+                    request.setAttribute("flashMessageFaild", "Identifiant must be number");
+                    this.getServletContext().getRequestDispatcher(isCentre ? "/index.jsp" : "/jsp/adminDashBoard.jsp").forward(request, response);
+                } else {
+                    int idCentre = Integer.parseInt(strIdCentre);
+                    Centre centre = centreDao.getCentre(idCentre);
+                    if (isCentre) {
+                        Centre centreConnecter = (Centre) session.getAttribute("centre");
+                        if (centreConnecter.getIdCentre() != centre.getIdCentre()) {
                             response.sendRedirect("/");
                             return;
                         }
                     }
-                    if(centre==null){
-                        request.setAttribute("flashMessageFaild","Wrong Identifiant");
-                        this.getServletContext().getRequestDispatcher(isCentre?"/index.jsp":"/jsp/adminDashBoard.jsp").forward(request,response);
-                    }else{
-                        request.setAttribute("centre",centre);
-                        List<Ville> villes=villeDao.getAllVille();
-                        request.setAttribute("villes",villes);
-                        this.getServletContext().getRequestDispatcher("/jsp/Centre/formulaireUpdateCentre.jsp").forward(request,response);
+                    if (centre == null) {
+                        request.setAttribute("flashMessageFaild", "Wrong Identifiant");
+                        this.getServletContext().getRequestDispatcher(isCentre ? "/index.jsp" : "/jsp/adminDashBoard.jsp").forward(request, response);
+                    } else {
+                        request.setAttribute("centre", centre);
+                        List<Ville> villes = villeDao.getAllVille();
+                        request.setAttribute("villes", villes);
+                        this.getServletContext().getRequestDispatcher("/jsp/Centre/formulaireUpdateCentre.jsp").forward(request, response);
                     }
                 }
             }
@@ -168,17 +167,17 @@ public class UpdateCentre extends HttpServlet {
     }
 
     private void returnFormulaire(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if(!isCentre){
-            List<Ville>villes=villeDao.getAllVille();
+        if (!isCentre) {
+            List<Ville> villes = villeDao.getAllVille();
             request.setAttribute("villes", villes);
         }
-        this.getServletContext().getRequestDispatcher("/jsp/Centre/formulaireUpdateCentre.jsp").forward(request,response);
+        this.getServletContext().getRequestDispatcher("/jsp/Centre/formulaireUpdateCentre.jsp").forward(request, response);
     }
 
-    private String validationField(String field,String pattern,String erreur){
-        if(field.matches(pattern)){
+    private String validationField(String field, String pattern, String erreur) {
+        if (field.matches(pattern)) {
             return "";
-        }else{
+        } else {
             return erreur;
         }
     }
